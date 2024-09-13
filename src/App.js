0f27e493592c1adcf9a4b3c82a8dd92ba601fe3d
amitkumar <amitkr9835@gmail.com>
2024-09-13 10:43:46 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ function App() {
             <div className="name">Manish Kumar</div>
             <div className="profile-desc">Hi, I am Manish Kumar UI/Front-End Developer. In my over 5 years of Development carrer, I have worked on 100+ Web Development Projects including Admin Dashboard, Web Applications, and Websites. Here are some list of projects where i have worked. Maybe some domain has been expired of freelance projects.  </div>
 
-            <div className="project-sec">
+
+            <div className="project-sec desktop">
               <div className="heading">React Js Projetcs</div>
 
               <div className="project-item-flex">
@@ -85,6 +86,67 @@ function App() {
               </div>
 
             </div>
+
+
+          {/* mobile view */}
+          <div className="project-sec desktop mobile ">
+              <div className="heading">React Js Projetcs</div>
+              <div className="project-item-flex">
+                <div className="project-name"><a href="https://manishnagar.github.io/weather_update/">Weather App</a></div>
+                <div className="project-name"><a href="https://manishnagar.github.io/react-movie-search-app/">Movie Search App</a></div>
+           
+              </div>
+              <div className="project-item-flex">
+              <div className="project-name"><a href="https://manish-kumar-profile.netlify.app/">Portfolio App</a></div>
+              </div>
+
+              <div className="heading">Front-End Projetcs</div>
+       
+              <div className="project-item-flex">
+              <div className="project-name"><a href="https://www.renewbuy.com/">Renewbuy.com</a></div>
+                <div className="project-name"><a href="https://www.renewbuyfinsure.com/renewbuy-health/#/buy-online">Renewbuy-Health</a></div>
+                </div>
+                <div className="project-item-flex">
+                <div className="project-name"><a href="https://www.renewbuyfinsure.com/renewbuy-suraksha/#/buy-online">Renewbuy-Life</a></div>
+                <div className="project-name"><a href="https://redcliffelabs.com/">Redcliffelabs.com</a></div>
+                </div>
+
+                <div className="project-item-flex">
+                <div className="project-name"><a href="https://www.agbeindia.com/">Agbe India Pvt Ltd.</a></div>
+                <div className="project-name"></div>
+              </div>
+              
+
+
+              <div className="heading">Freelancing Projetcs</div>
+              <div className="project-item-flex">
+                <div className="project-name"><a href="https://www.afinoz.com/">Afinoz Digital Finance</a></div>
+                <div className="project-name"><a href="https://www.abuildtech.com/">A Buildtech</a></div>
+                </div>
+                <div className="project-item-flex">
+                <div className="project-name"><a href="https://detoxa-frontend.netlify.app/">Detoxa</a></div>
+                <div className="project-name"><a href="https://rocaresolution.com/">ROCare Solution</a></div>
+              </div>
+
+              <div className="project-item-flex">
+                
+                <div className="project-name"><a href="https://spidcindia.com/">SPIDC India</a></div>
+                <div className="project-name"><a href="https://www.mithilainfrastructure.com/#">Mithila Infrastructure</a></div>
+              </div>
+
+              <div className="project-item-flex">
+                <div className="project-name"><a href="https://www.rupaybachao.com/index.html">Rupay Bachao</a></div>
+             
+
+
+              </div>
+
+            </div>
+
+
+
+
+
 
           </div>
         </div>
